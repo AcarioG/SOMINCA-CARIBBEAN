d75832d5d74086f36fabe3bf1f9899fbb8c9753a
wilmer vasquez de leon <wilmer@ceo.do>
2021-03-25 17:53:13 -0400

    Add project files.

---
+++ SOMINCA.Domain/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOMINCA.Domain.Models
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+
+
+    }
+}
---
+++ SOMINCA.Domain/Models/Reserva.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOMINCA.Domain.Models
+{
+    public class Reserva
+    {
+
+    }
+}
