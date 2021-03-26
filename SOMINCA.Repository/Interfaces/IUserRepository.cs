2ca77b4235032337d25623428bb10fa8daa14049
Wilmer Vasquez <cort-1998@hotmail.com>
2021-03-26 00:25:40 -0400

    UserRepository added

---
+++ SOMINCA.Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using SOMINCA.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOMINCA.Repository.Interfaces
+{
+    public interface IUserRepository : IBaseRepository<Usuario>
+    {
+        Task<IEnumerable<Usuario>> GetAllUsers();
+        Task<Usuario> GetUser(int Id);
+        Task AddUser(Usuario entity);
+        Task ModifyUser(Usuario entity);
+        Task DeleteUser(Usuario entity);
+        Task<bool> SaveUserDbAsync();
+        Task<bool> UserExistsAsync(int Id);
+    }
+}
---
+++ SOMINCA.Repository/Repositories/UserRepository.cs
@@ -0,0 +1,54 @@
+﻿using SOMINCA.Domain.Models;
+using SOMINCA.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOMINCA.Repository.Repositories
+{
+    public class UserRepository : BaseRepository<Usuario>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext context)
+            : base(context)
+        {
+
+        }
+        public async Task AddUser(Usuario entity)
+        {
+            await Insert(entity);
+        }
+
+        public async Task DeleteUser(Usuario entity)
+        {
+            await Delete(entity);
+        }
+
+        public async Task<IEnumerable<Usuario>> GetAllUsers()
+        {
+            return await GetAll();
+        }
+
+        public async Task<Usuario> GetUser(int Id)
+        {
+            return await Get(Id);
+        }
+
+        public async Task ModifyUser(Usuario entity)
+        {
+            await Update(entity);
+        }
+
+        public async Task<bool> SaveUserDbAsync()
+        {
+            return await Save();
+        }
+
+        public async Task<bool> UserExistsAsync(int Id)
+        {
+            var user = await GetAllUsers();
+            return user.Any(db => db.Id == Id);
+        }
+    }
+}
